8e0330b783ed35e53ffc2c47ac869a0fe7dedbed
fobos78 <57541130+fobos78@users.noreply.github.com>
2019-12-24 17:58:42 +0300

    failed to connect via web pack

--- code.js
+++ code.js
@@ -1,10 +1,24 @@
-window.addEventListener("scroll", function(){
-let squares = document.querySelectorAll(".square");
-    // если докрутили до 300 вылетает alert докрутил
-     if(window.pageYOffset > 1100){
-        for (let i = 0; i < squares.length; i++) {
-            squares[i].style.backgroundColor = "yellow";
+//module.exports = "Hello world!";
+ window.addEventListener("scroll", function(){
+
+    // если докрутили до 250 
+     if(window.pageYOffset > 250){
+        anime({
+            targets: 'div',
+            translateX: '70rem',
+            scale: [1, 1],
+            delay: function(el, index) {
+              return index * 1000;
+            },
             
-        }
+            
+          });
      }
 })
+
+//import 'anime.js';
+//const animejs = require('animejs');
+
+
+
+  //exports.module = alert("Hello")
